62535bb3cacb79c4cde5a97628388f3e4f5a3945
ahmad92894 <ahmad92894@gmail.com>
2023-04-09 16:00:39 -0500

    bcs push

---
+++ client/src/utils/query.js
@@ -0,0 +1,21 @@
+
+import { gql } from '@apollo/client';
+
+export const Get_me = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
